609141438f418f872c52a3bd7163a59de5f68e9e
DP <dp@DPs-MacBook-Pro.local>
2018-11-21 11:46:42 +0530

    Drag & drop script with unity library

---
+++ DragController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class DragController : MonoBehaviour, IPointerDownHandler, IDragHandler, IPointerUpHandler
+{
+    public RectTransform currentTransform;
+    private GameObject mainContent;
+    private Vector3 currentPossition;
+
+    private int totalChild;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        currentPossition = currentTransform.position;
+        mainContent = currentTransform.parent.gameObject;
+        totalChild = mainContent.transform.childCount;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        currentTransform.position =
+            new Vector3(currentTransform.position.x, eventData.position.y, currentTransform.position.z);
+
+        for (int i = 0; i < totalChild; i++)
+        {
+            if (i != currentTransform.GetSiblingIndex())
+            {
+                Transform otherTransform = mainContent.transform.GetChild(i);
+                int distance = (int) Vector3.Distance(currentTransform.position,
+                    otherTransform.position);
+                if (distance <= 10)
+                {
+                    Vector3 otherTransformOldPosition = otherTransform.position;
+                    otherTransform.position = new Vector3(otherTransform.position.x, currentPossition.y,
+                        otherTransform.position.z);
+                    currentTransform.position = new Vector3(currentTransform.position.x, otherTransformOldPosition.y,
+                        currentTransform.position.z);
+                    currentTransform.SetSiblingIndex(otherTransform.GetSiblingIndex());
+                    currentPossition = currentTransform.position;
+                }
+            }
+        }
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        currentTransform.position = currentPossition;
+    }
+}
